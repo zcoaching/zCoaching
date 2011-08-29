5005ac7fce39f5476de627a33dd195cde8c6a569
Tomas Rigaux <rigauxt@gmail.com>
2011-08-29 13:41:12 +0200

    optimisation du systeme grace a php

--- www/index.php
+++ www/index.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
-    <head>
-        <title>zCoaching</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="Content-Language" content="fr">
-        
-        <link rel="stylesheet" media="screen" type="text/css" title="Design" href="style.css" />
-        <!--[if IE 6]><link rel="stylesheet" media="screen" type="text/css" title="Design" href="ie6.css" /><![endif]-->
-        <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
-		
-		<!-- script pour ajouter un bouton +1 à mettre dans le head -->
-		<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
-		  {lang: 'fr'}
-		</script>
-    </head>
-
-    <body>
-            <div id="conteneur_1">
-               <div id="banniere">                       
-                       <div id="logo"><a href="./"><img src="images/logo_zcoaching.png" /></a></div>
-               </div>
-            </div>
+<?php
+$racine = "./"; // cette variable contient le lien vers la racine du site
+//en tete placée dans un autre fichier
+include ( $racine."includes/entete.php");
+?>
 
                     <!-- Début du menu horizontal -->
                     <ul id="MH">
