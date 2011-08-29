2b1a7dae4cb3a4d8fd61cc4f5760239ec8d8f1a5
Tomas Rigaux <rigauxt@gmail.com>
2011-08-29 15:23:17 +0200

    rassemblement des fichiers entete.php, menuH.php et menuV.php

--- www/includes/bottom.php
+++ www/includes/bottom.php
@@ -1,6 +1,13 @@
-<div id="pied_de_page">&copy; zCoaching<br />
+
+				
+				<div id="clear_both"></div>
+			</div>
+			<div id="pied_de_page">&copy; zCoaching<br />
         <br />
         <a href="#" onclick="alert('Indisponible');return false;"><img src="images/twitter.png" alt="Twitter" /></a>
         <g:plusone href="./index.html"></g:plusone><!-- bouton +1 -->
         <a href="#" onclick="alert('Indisponible');return false;"><img src="images/facebook.png" alt="Facebook" /></a>
         </div>
+        </div>
+	</body>
+</html>
---
+++ www/includes/top.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+    <head>
+        <title>zCoaching</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="Content-Language" content="fr">
+        
+        <link rel="stylesheet" media="screen" type="text/css" title="Design" href="style.css" />
+        <!--[if IE 6]><link rel="stylesheet" media="screen" type="text/css" title="Design" href="ie6.css" /><![endif]-->
+        <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+		
+		<!-- script pour ajouter un bouton +1 à mettre dans le head -->
+		<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
+		  {lang: 'fr'}
+		</script>
+    </head>
+
+    <body>
+            <div id="en_tete">
+               <div id="banniere">                       
+                       <div id="logo"><a href="./"><img src="images/logo_zcoaching.png" /></a></div>
+               </div>
+            </div>
+            <div id="conteneur_2">
+			<div id="corps">
+			<!-- Début du menu horizontal -->
+                    <ul id="MH">
+                        <li><a href="http://www.siteduzero.com/" onclick="window.open(this.href);return false;"><< Retour vers www.siteduzero.com</a>
+                        <li><a href="./">Accueil</a></li>
+                        <li><a href="#" onclick="alert('Indisponible');return false;">Forums</a></li>
+                    </ul>
+                    <!-- Fin du menu horizontal -->
+                    <!-- Début du menu vertical -->
+                    <div id="MV">
+                        
+                        <div class="en_tete_MV">Menu</div>
+                        <a href="#" class="lien_MV">Lien</a>
+                        <a href="#" class="lien_MV">Lien</a>
+                        <a href="#" class="lien_MV">Lien</a>
+                        <a href="#" class="lien_MV">Lien</a>
+                        <a href="#" class="lien_MV">Lien</a>
+                        <a href="#" class="lien_MV">Lien</a>
+                        <div class="marge_MV"></div>
+                        
+                    </div>
+                    <!--Fin du menu vertical -->
